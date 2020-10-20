f646a3e52eb3d55e71616af225cf7fa62148ce38
daisuke0207 <daisuke02074869@gmail.com>
2020-10-20 17:51:08 +0900

    connectモデルテストコードの実装

--- spec/factories/connects.rb
+++ spec/factories/connects.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :connect do
-    
+    title {"Google翻訳"}
+    link {"https://translate.google.co.jp/?hl=ja#view=home&op=translate&sl=en&tl=ja"}
+    position {1}
+    association :user
+    association :box
   end
 end
