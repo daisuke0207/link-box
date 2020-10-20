4f6e0b870233baef173084b77c82b3259315012d
daisuke0207 <daisuke02074869@gmail.com>
2020-10-20 17:16:31 +0900

    boxテストコードの実装

--- spec/models/box_spec.rb
+++ spec/models/box_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Box, type: :model do
-  
+  describe '#create' do
+    before do
+      @box = FactoryBot.build(:box)
+    end
+
+    it 'nameとposition、userが存在していれば登録できること' do
+      expect(@box).to be_valid
+    end
+
+    it 'nameが空では登録できないこと' do
+      @box.name = nil
+      @box.valid?
+      expect(@box.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it 'positionが空では登録できないこと' do
+      @box.position = nil
+      @box.valid?
+      expect(@box.errors.full_messages).to include("Position can't be blank")
+    end
+
+    it 'userが空では登録できないこと' do
+      @box.user = nil
+      @box.valid?
+      expect(@box.errors.full_messages).to include("User must exist")
+    end
+  end
 end
