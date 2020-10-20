5ae14db829d60464f60169d757cb8fc074bd9723
daisuke0207 <daisuke02074869@gmail.com>
2020-10-20 15:10:42 +0900

    box編集機能の実装

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -4,8 +4,22 @@ class BoxesController < ApplicationController
     @boxes = Box.includes(:user)
   end
 
+  def create
+    box = Box.create(name: params[:name], position: 1, user_id: current_user.id)
+    render json: { box: box }
+  end
+
   def edit
-    
+    @box = Box.find(params[:id])
+  end
+
+  def update
+    @box = Box.find(params[:id])
+    if @box.update(box_params)
+      redirect_to box_connects_path(@box)
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -17,12 +31,9 @@ class BoxesController < ApplicationController
     end
   end
 
-  def update
-    
-  end
+  private
 
-  def create
-    box = Box.create(name: params[:name], position: 1, user_id: current_user.id)
-    render json: { box: box }
+  def box_params
+    params.require(:box).permit(:name, :position).merge(user_id: current_user.id)
   end
 end
