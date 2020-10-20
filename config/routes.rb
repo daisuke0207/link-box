3685a17ac692f5895db5969b8b9757559fda7cce
daisuke0207 <daisuke02074869@gmail.com>
2020-10-20 16:08:31 +0900

    connect削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "boxes#index"
   resources :boxes, only: [:index, :edit, :destroy, :update, :create] do
-    resources :connects, only: [:index, :create]
+    resources :connects, only: [:index, :destroy, :create]
   end
 end
