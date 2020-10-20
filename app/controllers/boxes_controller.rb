1faae56d007c321f2c5e70fc0f0d80727d1ac38c
daisuke0207 <daisuke02074869@gmail.com>
2020-10-20 12:10:39 +0900

    box削除機能の実装

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -4,6 +4,23 @@ class BoxesController < ApplicationController
     @boxes = Box.includes(:user)
   end
 
+  def edit
+    
+  end
+
+  def destroy
+    box = Box.find(params[:id])
+    if box.destroy
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def update
+    
+  end
+
   def create
     box = Box.create(name: params[:name], position: 1, user_id: current_user.id)
     render json: { box: box }
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "boxes#index"
-  resources :boxes, only: [:index, :create] do
+  resources :boxes, only: [:index, :edit, :destroy, :update, :create] do
     resources :connects, only: [:index, :create]
   end
 end
