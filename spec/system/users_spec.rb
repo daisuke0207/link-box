74f7314ea9990d733a3f16c2be1cd494d879299e
daisuke0207 <daisuke02074869@gmail.com>
2020-10-21 11:39:09 +0900

    ユーザー新規登録結合テストコードの実装

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -4,26 +4,51 @@ RSpec.describe "Users", type: :system do
   before do
     @user = FactoryBot.build(:user)
   end
-  content 'ユーザー新規登録ができるとき' do
+  context 'ユーザー新規登録ができるとき' do
     it '正しい情報を入力すればユーザー新規登録ができてトップページに遷移する' do
       # トップページに移動する
+      visit root_path
       # トップページにサインアップページに遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
       # 新規登録ページに移動する
+      visit new_user_registration_path
       # ユーザー情報を入力する
+      fill_in 'ニックネーム', with: @user.nickname
+      fill_in 'メールアドレス', with: @user.email
+      fill_in 'パスワード', with: @user.password, match: :first
+      fill_in 'パスワード(確認)', with: @user.password_confirmation
       # サインアップボタンを押すとユーザーモデルのカウントが1上がることを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change {User.count}.by(1)
       # トップページに遷移する
+      expect(current_path).to eq root_path
       # ログアウトボタンが表示されていることを確認する
+      expect(page).to have_content('ログアウト')
       # サインアップページに遷移するボタンやログインボタンが非表示であることを確認する
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
     end
   end
-  content 'ユーザー新規登録ができないとき' do
+  context 'ユーザー新規登録ができないとき' do
     it '誤った情報ではユーザー新規登録ができずに新規登録ページに戻る' do
       # トップページに移動する
+      visit root_path
       # トップページにサインアップページに遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
       # 新規登録ページに移動する
+      visit new_user_registration_path
       # ユーザー情報を入力する
+      fill_in 'ニックネーム', with: ""
+      fill_in 'メールアドレス', with: ""
+      fill_in 'パスワード', with: "", match: :first
+      fill_in 'パスワード(確認)', with: ""
       # サインアップボタンを押してもユーザーモデルのカウントが上がらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change {User.count}.by(0)
       # 新規登録ページに戻されることを確認する
+      expect(current_path).to eq "/users"
     end
   end
 end
