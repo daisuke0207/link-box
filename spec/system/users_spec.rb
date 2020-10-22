c8a5b62a9ddd1d9bc3ac9047deaba6e41692122b
daisuke0207 <daisuke02074869@gmail.com>
2020-10-23 08:43:46 +0900

    basic認証を用いた新規登録テストコード

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,11 +1,18 @@
 require 'rails_helper'
 
+def basic_pass(path)
+  username = ENV["AUTH_USER"] 
+  password = ENV["AUTH_PASSWORD"]
+  visit "http://#{username}:#{password}@#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}#{path}"
+end
+
 RSpec.describe 'Users', type: :system do
   before do
     @user = FactoryBot.build(:user)
   end
   context 'ユーザー新規登録ができるとき' do
     it '正しい情報を入力すればユーザー新規登録ができてトップページに遷移する' do
+      basic_pass root_path
       # トップページに移動する
       visit root_path
       # トップページにサインアップページに遷移するボタンがあることを確認する
@@ -32,6 +39,7 @@ RSpec.describe 'Users', type: :system do
   end
   context 'ユーザー新規登録ができないとき' do
     it '誤った情報ではユーザー新規登録ができずに新規登録ページに戻る' do
+      basic_pass root_path
       # トップページに移動する
       visit root_path
       # トップページにサインアップページに遷移するボタンがあることを確認する
