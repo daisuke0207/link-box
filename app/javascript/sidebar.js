21edb345a69e1459478f906acd62f2340d1e8fc5
daisuke0207 <daisuke02074869@gmail.com>
2020-10-30 19:38:22 +0900

    JSの準備

---
+++ app/javascript/sidebar.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", () => {
+  const sideBtn = document.getElementById("side-view");
+  sideBtn.addEventListener("click", () => {
+    const sideBar = document.getElementById("side-bar");
+    sideBar.className="side-exit";
+  });
+});
+
+// function sideStatus() {
+
+// }
+
+// function sideOpen() {
+
+// }
+
+// function sideExit() {
+
+// }
+// window.addEventListener("load", sideStatus);
