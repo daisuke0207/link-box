45894999a483d304fc4b43ff617420b035208ae9
daisuke0207 <daisuke02074869@gmail.com>
2020-10-31 18:04:04 +0900

    ページを限定した

--- app/javascript/sidebar.js
+++ app/javascript/sidebar.js
@@ -1,19 +1,21 @@
 function sideStatus() {
-  const sideBtn = document.getElementById("side-view");
-  if (sideBtn.getAttribute("data-load") != null ) {
-    return null;
-  }
-  sideBtn.setAttribute("data-load", "true");
-  let bool = false;
-  sideBtn.addEventListener("click", () => {
-    const side_status = document.getElementById("side-bar");
-    if (bool) {
-      side_status.className="side-bar";
-      bool = false;
-    } else {
-      side_status.className="side-exit";
-      bool = true;
+  if (document.URL.match(/boxes/)) {
+    const sideBtn = document.getElementById("side-view");
+    if (sideBtn.getAttribute("data-load") != null ) {
+      return null;
     }
-  });
+    sideBtn.setAttribute("data-load", "true");
+    let bool = false;
+    sideBtn.addEventListener("click", () => {
+      const side_status = document.getElementById("side-bar");
+      if (bool) {
+        side_status.className="side-bar";
+        bool = false;
+      } else {
+        side_status.className="side-exit";
+        bool = true;
+      }
+    });
+  }
 }
 setInterval(sideStatus, 1000);
