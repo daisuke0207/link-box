507de3cafeb431cd71eef22372cc30ff04004397
daisuke0207 <daisuke02074869@gmail.com>
2020-11-05 15:56:44 +0900

    コード整理

--- app/javascript/sidebar.js
+++ app/javascript/sidebar.js
@@ -1,3 +1,4 @@
+// サイドバー開閉操作
 function sideStatus() {
   if (document.URL.match(/boxes/)) {
     const sideBtn = document.getElementById("side-view");
@@ -9,13 +10,24 @@ function sideStatus() {
     sideBtn.addEventListener("click", () => {
       const side_status = document.getElementById("side-bar");
       if (bool) {
-        side_status.className="side-bar";
+        open(side_status);
         bool = false;
       } else {
-        side_status.className="side-exit";
+        close(side_status);
         bool = true;
       }
     });
   }
 }
+
+// サイドバーを開く
+function open(status){
+  status.className="side-bar";
+}
+
+// サイドバー閉じる
+function close(status) {
+  status.className="side-close";
+}
+
 setInterval(sideStatus, 1000);
