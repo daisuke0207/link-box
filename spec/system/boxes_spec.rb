71e33aaf6ce8b2e38103013189743dfd806a724c
daisuke0207 <daisuke02074869@gmail.com>
2020-11-15 11:25:50 +0900

    boxシステムテストの概要

--- spec/system/boxes_spec.rb
+++ spec/system/boxes_spec.rb
@@ -1,6 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe "Boxes", type: :system do
-  before do
+  describe '一覧表示機能' do
+    before do
+      # ユーザーAを作成
+      # ユーザーAのboxを作成
+    end
+    
+    context 'ユーザーAがログインしているとき' do
+      it 'ユーザーAが作成したboxが表示されていること' do
+        # 作成済みのbox名が画面に表示されていることを確認
+      end
+    end
+
+    context 'ユーザーBがログインしているとき' do
+      before do
+        # ユーザーBを作成
+        # ユーザーBでログイン
+      end
+
+      it 'ユーザーAが作成したboxが表示されていないこと' do
+        # ユーザーAが作成したbox名が画面に表示されていないこと
+      end
+    end
   end
 end
