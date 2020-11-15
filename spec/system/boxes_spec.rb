9a8f54f0b4b974bcf931247c0f8fbcb501f203b5
daisuke0207 <daisuke02074869@gmail.com>
2020-11-15 12:20:45 +0900

    box一覧表示のテスト

--- spec/system/boxes_spec.rb
+++ spec/system/boxes_spec.rb
@@ -3,20 +3,31 @@ require 'rails_helper'
 RSpec.describe "Boxes", type: :system do
   describe '一覧表示機能' do
     before do
-      # ユーザーAを作成
-      # ユーザーAのboxを作成
+      @user_a = FactoryBot.create(:user, nickname: 'ユーザーA', email: 'a@com')
+      FactoryBot.create(:box, name: 'Aのbox', user: @user_a)
     end
     
     context 'ユーザーAがログインしているとき' do
+      before do
+        visit root_path
+        visit new_user_session_path
+        fill_in 'メールアドレス', with: @user_a.email
+        fill_in 'パスワード', with: @user_a.password
+        click_on 'ログイン'
+      end
       it 'ユーザーAが作成したboxが表示されていること' do
-        # 作成済みのbox名が画面に表示されていることを確認
+        expect(page).to have_content('Aのbox')
       end
     end
 
     context 'ユーザーBがログインしているとき' do
       before do
         # ユーザーBを作成
-        # ユーザーBでログイン
+        # visit root_path
+        # visit new_user_session_path
+        # fill_in 'メールアドレス', with: user_a.email
+        # fill_in 'パスワード', with: user_a.password
+        # click_on 'ログイン'
       end
 
       it 'ユーザーAが作成したboxが表示されていないこと' do
