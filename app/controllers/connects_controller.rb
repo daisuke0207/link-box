57ad43b370b4ceda2564b1be0d24dee4506f0693
daisuke0207 <daisuke02074869@gmail.com>
2020-11-24 14:31:42 +0900

    コード整理

--- app/controllers/connects_controller.rb
+++ app/controllers/connects_controller.rb
@@ -83,19 +83,7 @@ class ConnectsController < ApplicationController
 
   def destroy_all_history(connects)
     connects.each do |connect|
-      new_history = Deletehistory.new
-      new_history.user_id = current_user.id
-      new_history.title = connect.title
-      new_history.link = connect.link
-
-      if Deletehistory.find_by(link: new_history.link)
-        old_history = Deletehistory.find_by(link: new_history.link)
-        old_history.destroy
-      end
-      new_history.save
-      histories_stock_limit = 20
-      histories = Deletehistory.all
-      histories[0].destroy if histories.count > histories_stock_limit
+      delete_history(connect)
     end
   end
 end
