48ff7a0e2ed5adb0da19dbf2c12be38661944e87
daisuke0207 <daisuke02074869@gmail.com>
2020-11-28 17:43:03 +0900

    ユーザーメニューの実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,7 +13,6 @@ require("../new_connect")
 require("../add_connect")
 require("../all_open")
 require("../sidebar")
-require("../user_menu")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
