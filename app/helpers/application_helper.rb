95973b9b442c651c678cb1c8ac6c7d9872cc9fe0
daisuke0207 <daisuke02074869@gmail.com>
2021-01-06 14:07:17 +0900

    Fix : URL setthing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,4 @@
 module ApplicationHelper
-  require 'uri'
-
-  def text_url_to_link(text)
-    URI.extract(text, %w[http https]).uniq.each do |url|
-      text.gsub!(url)
-    end
-    text
-  end
 
   def delete_history_get
     delete_histories = Deletehistory.includes(:user).order(id: 'DESC')
