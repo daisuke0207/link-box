f96ed1f665d7b80ec9ce7169ac99554df1060d5e
daisuke0207 <daisuke02074869@gmail.com>
2021-02-01 12:07:11 +0900

    修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def delete_history_get
-    delete_histories = current_user.Deletehistory.includes(:user).order(id: 'DESC')
+    delete_histories = Deletehistory.includes(:user).order(id: 'DESC')
     delete_histories
   end
 end
